0694b23571e76780c52b5c58006272a08bbd9fb5
Jesús Jimenez Acosta <chus1998d@gmail.com>
2023-03-29 18:06:59 -0600

    Prueba

--- BINGO.c
+++ BINGO.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <stdbool.h>
 //#include <pic14/pic12f675.h>
-#include <pic14/pic12f683.h>
+#include <C:/Program Files/SDCC/include/pic14/pic12f683.h>
 //#include <xc.h>
 typedef unsigned int word;
 word __at 0x2007 __CONFIG = (_WDTE_OFF & _WDT_OFF & _MCLRE_OFF);  // WDT y MCLR OFF
@@ -24,7 +24,7 @@ struct Numero_total {
   int Decen;
 };
 
-struct Numero_total numero_aleatorio() {
+struct Numero_total* numero_aleatorio() {
     struct Numero_total numero;
     int aux = 0 ;
     unsigned int bit;
@@ -38,50 +38,10 @@ struct Numero_total numero_aleatorio() {
     printf("num1 = %d",aux);
     numero.Unid = aux % 10;
     numero.Decen = (aux / 10) % 10;
-    return numero;
+    return &numero;
 }
 
 
-/*
-bool isNumberRepeated(unsigned char num) {
-    unsigned char i, j;
-    unsigned char data[128]; // asumiendo memoria en PIC12F683 de 128 bytes
-
-    // leer la memoria
-    for(i = 0; i < 128; i++) {
-        data[i] = _data_read(i);
-    }
-
-    // revisa si el numero está repetido
-    for(i = 0; i < 128; i++) {
-        for(j = i + 1; j < 128; j++) {
-            if(data[i] == data[j] && data[i] == num) {
-                return true; // numero repetido
-            }
-        }
-    }
-
-    return false; // numero no repetido
-}
-
-
-
-
-void guardar_imprimir(){
-    bool a=true;
-    struct Numero_total n;
-    while (a=true){
-        //n=numero_aleatorio();
-        //a=func(); //funcion para ver si a está en los registros
-        if (a==false){
-            //funcion_escribir_imprimir();
-        }
-        else{
-
-        }
-    }
-}
-*/
 
 void main(void) {
     //int TRISIObits.TRISIO3 = 0b1;     // Se configura gp3 como entrada
@@ -98,9 +58,8 @@ void main(void) {
                 //codigo para verificar si el numero está dentro del registro
                 //usar una funcion para ver si el numero calculado está en el registro,salida booleana
                 //guardar_imprimir();
-                struct Numero_total n;
-                n=numero_aleatorio();
-                if(n.Unid == 0){
+                struct Numero_total* n=numero_aleatorio();
+                if(n->Unid == 0){
                     bit0 = 0;
                     bit1 = 0;
                     bit2 = 0;
@@ -108,28 +67,28 @@ void main(void) {
                     continue;
                 }
 
-                if(n.Unid == 1){
+                if(n->Unid == 1){
                     bit0 = 1;
                     bit1 = 0;
                     bit2 = 0;
                     bit3 = 0;
                     continue;
                 }
-                if(n.Unid == 2){
+                if(n->Unid == 2){
                     bit0 = 0;
                     bit1 = 1;
                     bit2 = 0;
                     bit3 = 0;
                     continue;
                 }
-                if(n.Unid == 3){
+                if(n->Unid == 3){
                     bit0 = 1;
                     bit1 = 1;
                     bit2 = 0;
                     bit3 = 0;
                     continue;
                 }
-                if(n.Unid == 4)
+                if(n->Unid == 4)
                 {
                     bit0 = 0;
                     bit1 = 0;
@@ -137,7 +96,7 @@ void main(void) {
                     bit3 = 0;
                     continue;
                 }
-                if(n.Unid == 5)
+                if(n->Unid == 5)
                 {
                     bit0 = 1;
                     bit1 = 0;
@@ -145,7 +104,7 @@ void main(void) {
                     bit3 = 0;
                     continue;
                 }
-                if(n.Unid == 6)
+                if(n->Unid == 6)
                 {
                     bit0 = 0;
                     bit1 = 1;
@@ -154,7 +113,7 @@ void main(void) {
                     continue;
                 }
 
-                if (n.Unid == 7){
+                if (n->Unid == 7){
                     bit0 = 1;
                     bit1 = 1;
                     bit2 = 1;
@@ -162,7 +121,7 @@ void main(void) {
                     continue;
                 }
 
-                if (n.Unid == 8){
+                if (n->Unid == 8){
                     bit0 = 0;
                     bit1 = 0;
                     bit2 = 0;
@@ -170,14 +129,14 @@ void main(void) {
                     continue;
                 }
 
-                if (n.Unid == 9){
+                if (n->Unid == 9){
                     bit0 = 1;
                     bit1 = 0;
                     bit2 = 0;
                     bit3 = 1;
                     continue;
                 }
-                if(n.Decen == 0){
+                if(n->Decen == 0){
                     bit4 = 0;
                     bit5 = 0;
                     bit6 = 0;
@@ -185,28 +144,28 @@ void main(void) {
                     continue;
                 }
 
-                if(n.Decen == 1){
+                if(n->Decen == 1){
                     bit4 = 1;
                     bit5 = 0;
                     bit6 = 0;
                     bit7 = 0;
                     continue;
                 }
-                if(n.Decen == 2){
+                if(n->Decen == 2){
                     bit4 = 0;
                     bit5 = 1;
                     bit6 = 0;
                     bit7 = 0;
                     continue;
                 }
-                if(n.Decen == 3){
+                if(n->Decen == 3){
                     bit4 = 1;
                     bit5 = 1;
                     bit6 = 0;
                     bit7 = 0;
                     continue;
                 }
-                if(n.Decen == 4)
+                if(n->Decen == 4)
                 {
                     bit4 = 0;
                     bit5 = 0;
@@ -214,7 +173,7 @@ void main(void) {
                     bit7 = 0;
                     continue;
                 }
-                if(n.Decen == 5)
+                if(n->Decen == 5)
                 {
                     bit4 = 1;
                     bit5 = 0;
@@ -222,7 +181,7 @@ void main(void) {
                     bit7 = 0;
                     continue;
                 }
-                if(n.Decen == 6)
+                if(n->Decen == 6)
                 {
                     bit4 = 0;
                     bit5 = 1;
@@ -231,7 +190,7 @@ void main(void) {
                     continue;
                 }
 
-                if (n.Decen == 7){
+                if (n->Decen == 7){
                     bit4 = 1;
                     bit5 = 1;
                     bit6 = 1;
@@ -239,7 +198,7 @@ void main(void) {
                     continue;
                 }
 
-                if (n.Decen == 8){
+                if (n->Decen == 8){
                     bit4 = 0;
                     bit5 = 0;
                     bit6 = 0;
@@ -247,7 +206,7 @@ void main(void) {
                     continue;
                 }
 
-                if (n.Decen == 9){
+                if (n->Decen == 9){
                     bit4 = 1;
                     bit5 = 0;
                     bit6 = 0;
